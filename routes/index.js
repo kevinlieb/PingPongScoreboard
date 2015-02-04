72c17c6d7c3860562227d66ce4832910f6d223f1
Tom Fitch <tmjfitch@gmail.com>
2015-02-04 12:07:48 -0800

    couple fixes: (1) fix to package.json because it was malformed json (2) some routes that look like what we want but don't actually do anything

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,35 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/score/game_id', function (req, res, next) {
+	var score = {};
+	score.P1 = 7;
+	score.P2 = 1;
+	res.send(score);
+});
+
+router.post('/score/game_id', function (req, res) {
+	var score = req;
+	res.send('ok');
+});
+
+router.get('/list', function (req, res) {
+	var list = [];
+	list.push({
+		"Game": 1,
+		"P1": 0,
+		"P2": 0
+	});
+	list.push({
+		"Game": 2,
+		"P1": 5,
+		"P2": 7
+	});
+	res.send({Games: list});
+})
 
 module.exports = router;
