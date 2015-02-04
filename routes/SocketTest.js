26c4bbd28399e717ac23dd48f406521e04d97563
kevinlieb <kkevinl@yahoo.com>
2015-02-04 14:53:31 -0800

    update

--- routes/SocketTest.js
+++ routes/SocketTest.js
@@ -1,5 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var io = require('socket.io')(http);
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
 
 router.get("/", function(rec, res, next) {
   console.log("i am in SocketTest.js");
