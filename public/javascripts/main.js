52e57d3f8e58755d27624bce7a3c3df462c5a956
Patrick Hulce <patrick@brightroll.com>
2015-02-04 16:52:42 -0800

    added view choice links

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,27 @@
 $(document).ready(function () {
 
+	function newGameDialog() {
+		$.ajax({
+			url: '/new',
+			type: 'PUT'
+		}).done(function(s) {
+			showViewDialog(s.ok);
+		});
+	}
 
+	function showViewDialog(game_id) {
+		$('#main_cont').hide();
+		$('#choice_dialog').show();
+		$('.viewLink a').each(function() {
+			var $elem = $(this);
+			$elem.attr('href', $elem.attr('href') + game_id);
+		});
+	}
+
+	$('#new_game_cont button').click(newGameDialog);
+	$('.joinLink').click(function(evt) {
+		var game_id = $(this).data('game-id');
+		showViewDialog(game_id);
+	});
 
 });
--- public/javascripts/phoneview.js
+++ public/javascripts/phoneview.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-
+	var player = getUrlParameter('player');
+	var gameId = getUrlParameter('game_id');
 
 
 });
