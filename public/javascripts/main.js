7a676c10d9dfaf959836b91d71b2536e46926d81
Tom Fitch <tmjfitch@gmail.com>
2015-02-04 16:53:16 -0800

    get list from api

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
 
+	var gamesListItemTemplate = $('.joinLink').remove();
 
+	$.get('/list', function (gamesList) {
+		if (gamesList && gamesList.Games && gamesList.Games.length) {
+			gamesList = gamesList.Games;
+			var container = $('#join_game_list ul'),
+				game,
+				newGamesListNode,
+				i;
+			for (i = 0; i < gamesList.length; i++) {
+				game = gamesList[i];
+				newGamesListNode = gamesListItemTemplate.clone().attr('data-game-id', game.id).html('game ' + game.id + ', currently ' + game.P1 + ' - ' + game.P2);
+				container.append(newGamesListNode);
+			}
+		}
+	});
 
 });
